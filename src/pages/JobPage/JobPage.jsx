c76325388e93c7169e0731cd02bfc1debbd76610
Andrew Dimes <andrewdimes@gmail.com>
2021-06-01 12:49:58 -0400

    add calender for log income

--- src/pages/JobPage/JobPage.jsx
+++ src/pages/JobPage/JobPage.jsx
@@ -2,11 +2,14 @@ import React, { useState, useEffect } from 'react'
 import Header from '../../components/Header/Header';
 import { useHistory } from 'react-router-dom';
 import jobService from '../../utils/jobService'
+import Calendar from 'react-calendar'
 
 const JobPage = () => {
     const [error, setError] = useState('')
     const [job, setJob] = useState({})
     const [jobSwitch, setJobSwitch] = useState(false)
+    const [value, onChange] = useState(new Date());
+    const [logIncome, setLogIncome] = useState(false)
 
 
 
@@ -31,19 +34,27 @@ const JobPage = () => {
     function goBack() {
         window.history.back()
     }
+    function goBackLog() {
+        setLogIncome(false)
+    }
 
     return (
         <>
-            <Header job={job} jobSwitch={jobSwitch} jobPage={true} goBack={goBack} />
+            {logIncome ? <Header job={job} jobSwitch={jobSwitch} jobPage={true} goBack={goBackLog} /> : <Header job={job} jobSwitch={jobSwitch} jobPage={true} goBack={goBack} />}
             <br />
-            <h1>Just finished work?</h1>
-            <button class="ui primary button">
+            {logIncome ? '' :
+            <h1>Just finished work?</h1>}
+            {logIncome ? '' :<button onClick={() => {setLogIncome(true)}}class="ui primary button">
                 Log Income
-            </button><br /><br />
-            <h1>How much have you earned?</h1>
-            <button class="ui primary button">
+            </button> } <br /><br />
+            {logIncome ? <Calendar
+                onChange={onChange}
+                value={value}
+            /> : '' }
+            {logIncome ? '' : <h1>How much have you earned?</h1>}
+            {logIncome ? '' : <button class="ui primary button">
                 View Income
-            </button>
+            </button> }
         </>
     )
 }
