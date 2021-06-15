e27ccd9be9a3e792875f1e753b1d9b7e3d2839ff
Andrew Dimes <andrewdimes@gmail.com>
2021-06-15 11:46:44 -0400

    fix merge

--- src/pages/JobPage/JobPage.jsx
+++ src/pages/JobPage/JobPage.jsx
@@ -28,21 +28,21 @@ const JobPage = ({ handleLogOut }) => {
     const [yearView, setYearView] = useState(false)
 
 
-  async function getJob() {
-    try {
-      const data = await jobService.getJob(
-        window.location.pathname.substring(1)
-      );
-      setJob(data);
-      setJobSwitch(true);
-      // Route to wherever you want!
-    } catch (err) {
-      // Invalid user data (probably duplicate email)
-      setError(err.message);
+    async function getJob() {
+        try {
+            const data = await jobService.getJob(
+                window.location.pathname.substring(1)
+            );
+            setJob(data);
+            setJobSwitch(true);
+            // Route to wherever you want!
+        } catch (err) {
+            // Invalid user data (probably duplicate email)
+            setError(err.message);
+        }
     }
-  }
-  
-      async function getWages() {
+
+    async function getWages() {
         try {
             const data = await wageService.getWages(window.location.pathname.substring(1), viewBy)
             console.log(data)
@@ -51,7 +51,7 @@ const JobPage = ({ handleLogOut }) => {
             setError(err.message)
         }
     }
-      useEffect(() => {
+    useEffect(() => {
         getJob()
     }, [jobSwitch])
     useEffect(() => {
@@ -88,15 +88,15 @@ const JobPage = ({ handleLogOut }) => {
     }
 
     function theWageView(select) {
-        if(select === 'm'){
+        if (select === 'm') {
             setMonthView(true)
             setWeekView(false)
             setYearView(false)
-        } else if(select === 'w'){
+        } else if (select === 'w') {
             setWeekView(true)
             setMonthView(false)
             setYearView(false)
-        } else if(select === 'y'){
+        } else if (select === 'y') {
             setYearView(true)
             setWeekView(false)
             setMonthView(false)
@@ -110,17 +110,17 @@ const JobPage = ({ handleLogOut }) => {
         const year = date.toLocaleString('default', { year: 'numeric' });
         setYear(year)
         setMonth(month)
-        function setToMonday( date ) {
-            let day = date.getDay() || 7;  
-            if(day !== 1){
+        function setToMonday(date) {
+            let day = date.getDay() || 7;
+            if (day !== 1) {
                 date.setHours(-24 * (day - 1));
             }
-                 
+
             return date;
         }
-        function setToSunday( date ){
+        function setToSunday(date) {
             let day = date.getDay() || 7;
-            if(day !== 7){
+            if (day !== 7) {
                 date.setHours(24 * (day + 1))
             }
             return date;
@@ -129,15 +129,15 @@ const JobPage = ({ handleLogOut }) => {
         let sundayRaw = setToSunday(new Date())
         function getFormattedDate(date) {
             let year = date.getFullYear();
-          
+
             let month = (1 + date.getMonth()).toString();
             month = month.length > 1 ? month : '0' + month;
-          
+
             let day = date.getDate().toString();
             day = day.length > 1 ? day : '0' + day;
-            
+
             return month + '/' + day + '/' + year;
-          }
+        }
         let mondayFormatted = getFormattedDate(mondayRaw)
         let sundayFormatted = getFormattedDate(sundayRaw)
         setMonday(mondayFormatted)
@@ -147,96 +147,96 @@ const JobPage = ({ handleLogOut }) => {
 
 
 
-if (viewIncome !== true) {
-  if (wageFormView !== true) {
-    return (
-      <>
-        {logIncome ? (
-          <Header
-            handleLogOut={handleLogOut}
-            job={job}
-            jobSwitch={jobSwitch}
-            jobPage={true}
-            goBack={() => {
-              setLogIncome(false);
-            }}
-          />
-        ) : (
-          <Header
-            handleLogOut={handleLogOut}
-            job={job}
-            jobSwitch={jobSwitch}
-            jobPage={true}
-            goBack={goBack}
-          />
-        )}
-        <div className="job-page__item-container">
-          <div className="job-page__item">
-            {logIncome ? (
-              ''
-            ) : (
-              <h1 className="job-page__header">Just finished work?</h1>
-            )}
-            {logIncome ? (
-              ''
-            ) : (
-              <Btn
-                label="Log Income"
-                onClick={() => {
-                  setLogIncome(true);
-                }}
-              />
-            )}
-          </div>
-          <div className="job-page__item">
-            {logIncome ? <Calendar onChange={onChange} value={value} /> : ''}
-            <br />
-            {logIncome ? (
-              <Btn
-                onClick={() => {
-                  setWageFormView(true);
-                }}
-                label="Enter"
-              />
-            ) : (
-              ''
-            )}
-            {logIncome ? (
-              ''
-            ) : (
-              <h1 className="job-page__header">How much have you earned?</h1>
-            )}
-            {logIncome ? '' : <Btn onClick={() => { setViewIncome(true) }} label="View Income" />}
-          </div>
-        </div>
-      </>
-    );
-  } else {
-    return (
-      <>
-        <Header
-          handleLogOut={handleLogOut}
-          job={job}
-          jobSwitch={jobSwitch}
-          jobPage={true}
-          goBack={() => {
-            setWageFormView(false);
-          }}
-        />
-        <WageForm wageFormSubmit={wageFormSubmit} />
-      </>
-    );
-   } else {
-      
-        return (
-            <>
-                <Header goBack={() => { setViewIncome(false) }} handleLogOut={handleLogOut} job={job} jobSwitch={jobSwitch} jobPage={true} />
-                <WageDetail monday={monday} sunday={sunday} year={year} month={month} wageData={wageData} theWageView={theWageView} weekView={weekView} monthView={monthView} yearView={yearView} />
-            </>
-        )
+    if (viewIncome !== true) {
+        if (wageFormView !== true) {
+            return (
+                <>
+                    {logIncome ? (
+                        <Header
+                            handleLogOut={handleLogOut}
+                            job={job}
+                            jobSwitch={jobSwitch}
+                            jobPage={true}
+                            goBack={() => {
+                                setLogIncome(false);
+                            }}
+                        />
+                    ) : (
+                        <Header
+                            handleLogOut={handleLogOut}
+                            job={job}
+                            jobSwitch={jobSwitch}
+                            jobPage={true}
+                            goBack={goBack}
+                        />
+                    )}
+                    <div className="job-page__item-container">
+                        <div className="job-page__item">
+                            {logIncome ? (
+                                ''
+                            ) : (
+                                <h1 className="job-page__header">Just finished work?</h1>
+                            )}
+                            {logIncome ? (
+                                ''
+                            ) : (
+                                <Btn
+                                    label="Log Income"
+                                    onClick={() => {
+                                        setLogIncome(true);
+                                    }}
+                                />
+                            )}
+                        </div>
+                        <div className="job-page__item">
+                            {logIncome ? <Calendar onChange={onChange} value={value} /> : ''}
+                            <br />
+                            {logIncome ? (
+                                <Btn
+                                    onClick={() => {
+                                        setWageFormView(true);
+                                    }}
+                                    label="Enter"
+                                />
+                            ) : (
+                                ''
+                            )}
+                            {logIncome ? (
+                                ''
+                            ) : (
+                                <h1 className="job-page__header">How much have you earned?</h1>
+                            )}
+                            {logIncome ? '' : <Btn onClick={() => {console.log('test')}} label="View Income" />}
+                        </div>
+                    </div>
+                </>
+            );
+        } else {
+            return (
+                <>
+                    <Header
+                        handleLogOut={handleLogOut}
+                        job={job}
+                        jobSwitch={jobSwitch}
+                        jobPage={true}
+                        goBack={() => {
+                            setWageFormView(false);
+                        }}
+                    />
+                    <WageForm wageFormSubmit={wageFormSubmit} />
+                </>
+            );
+        }
+     } else {
 
-    }  
-};
+            return (
+                <>
+                    <Header goBack={() => { setViewIncome(false) }} handleLogOut={handleLogOut} job={job} jobSwitch={jobSwitch} jobPage={true} />
+                    <WageDetail monday={monday} sunday={sunday} year={year} month={month} wageData={wageData} theWageView={theWageView} weekView={weekView} monthView={monthView} yearView={yearView} />
+                </>
+            )
+        }
+    }
 
-      
+
 export default JobPage;
