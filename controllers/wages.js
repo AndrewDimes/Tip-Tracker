27af41933293bd03d4a626450c596183bffda043
Andrew Dimes <andrewdimes@gmail.com>
2021-06-24 12:01:38 -0400

    show wages by week/month/year working

--- controllers/wages.js
+++ controllers/wages.js
@@ -25,19 +25,65 @@ module.exports = {
   }
 
   async function getWages(req, res){
+    const date = new Date();  // 2009-11-10
+    const month = date.toLocaleString('default', { month: 'long' });
+    const year = date.toLocaleString('default', { year: 'numeric' });
+    function setToMonday(date) {
+        let day = date.getDay() || 7;
+        if (day !== 1) {
+            date.setHours(-24 * (day - 1));
+        }
+
+        return date;
+    }
+    function setToSunday(date) {
+        let day = date.getDay() || 7;
+        if (day !== 7) {
+            date.setHours(24 * (day - 1))
+        }
+        return date;
+    }
+    let monday = setToMonday(new Date())
+    let sunday = setToSunday(new Date())
+    sunday.setHours(00,00,00,00)
+    monday.setHours(00,00,00,00)
+    console.log(month)
     const job = await Job.findById(req.params.id)
     const user = await User.findOne({ _id: req.user._id });
-    console.log(req.params.viewBy,'in controller')
     let totalTips = 0
     let totalWages = 0
     let totalHours = 0
     let totalIncome = 0
-    for(let i=0; i < job.wages.length; i++){
-      console.log(job.wages[i].tips, 'in loop')
-      totalTips += job.wages[i].tips
-      totalWages += job.wages[i].wage
-      totalHours += job.wages[i].hours
-      totalIncome = totalTips + totalWages
+    if(req.params.viewBy === 'w'){
+      for(let i=0; i<job.wages.length;i++){
+        if(job.wages[i].date <= sunday && job.wages[i].date >= monday){
+          console.log(job.wages[i].date.toLocaleString('default', {month: 'long'}))
+          totalTips += job.wages[i].tips
+          totalWages += job.wages[i].wage
+          totalHours += job.wages[i].hours
+          totalIncome = totalTips + totalWages
+        }
+      }
+    } else if(req.params.viewBy === 'm'){
+      for(let i=0; i<job.wages.length;i++){
+        if(job.wages[i].date.toLocaleString('default', {month: 'long'}) === 'June'){
+          totalTips += job.wages[i].tips
+          totalWages += job.wages[i].wage
+          totalHours += job.wages[i].hours
+          totalIncome = totalTips + totalWages
+        }
+      }
+    } else if(req.params.viewBy === 'y'){
+      for(let i=0; i<job.wages.length;i++){
+        if(job.wages[i].date.toLocaleString('default', {year: 'numeric'}) === '2021'){
+          totalTips += job.wages[i].tips
+          totalWages += job.wages[i].wage
+          totalHours += job.wages[i].hours
+          totalIncome = totalTips + totalWages
+          averageIncome = totalIncome / totalHours
+          console.log(averageIncome)
+        }
+      }
     }
     try {
       const jobs = await Job.find({user:req.user})
@@ -45,8 +91,6 @@ module.exports = {
     } catch (err) {
       res.json(err)
     }
-
-    console.log(totalTips, 'tip total', totalWages, 'wage', totalHours,'hours', totalIncome, 'income')
   }
 
   function createJWT(user) {
