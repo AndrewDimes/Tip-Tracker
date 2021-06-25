5940b81b97fdf51ca60eeab88550be8537399bb9
Andrew Dimes <andrewdimes@gmail.com>
2021-06-24 21:58:44 -0400

    install d3

---
+++ src/components/Wage/WageChart.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const WageChart = () => {
+    return (
+        <div>
+            <h1 style={{color: 'white'}}>CHART GOES HERE</h1><br />
+        </div>
+    )
+}
+
+export default WageChart
+
